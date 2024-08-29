e6ce593c63741cb1aaea883a3d4a04f26b3f7d30
“NitinHegde2002” <hegde.nitin@yahoo.com>
2024-08-29 14:42:30 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -16,10 +16,31 @@ export default function Home() {
     });
   }, []);
 
+  const isMobileBrowser = () => {
+    return /Mobi|Android/i.test(navigator.userAgent);
+  };
+
   const showChromeNotification = async (windowTitle, header, details) => {
     const playSound = () => {
       const audio = new Audio("/sounds/happy-bells.wav");
-      audio.loop = true; // Loop the audio
+
+      if (isMobileBrowser()) {
+        let playCount = 0;
+        const maxPlays = 2;
+
+        audio.loop = true;
+
+        audio.addEventListener("ended", () => {
+          playCount += 1;
+          if (playCount >= maxPlays) {
+            audio.pause();
+            audio.currentTime = 0; // Reset the audio
+          }
+        });
+      } else {
+        audio.loop = true;
+      }
+
       audio.play().catch((error) => {
         console.error("Sound playback was blocked:", error);
       });
@@ -31,11 +52,20 @@ export default function Home() {
     if (permission === "granted") {
       const audio = playSound(); // Start playing sound and get the audio object
 
-      const notification = new Notification(`${windowTitle} : ${header}`, {
+      const notificationOptions = {
         body: details,
         icon: "/images/notiflogo.ico",
-        requireInteraction: true, // Keeps the notification on screen until user interacts
-      });
+      };
+
+      // Modify notification for desktop browsers
+      if (!isMobileBrowser()) {
+        notificationOptions.requireInteraction = true; // Keeps the notification on screen until user interacts
+      }
+
+      const notification = new Notification(
+        `${windowTitle} : ${header}`,
+        notificationOptions
+      );
 
       // Stop audio when notification is clicked
       notification.onclick = () => {
